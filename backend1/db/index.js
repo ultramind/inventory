11f87a7a3a4ce94ddfd0df1096a632e729ded60c
ultragrace <udechukwuakachukwu.com>
2023-01-11 23:29:42 +0100

    Folder structure and setup

--- backend1/db/index.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const mongoConnect = mongoose
-  .connect(process.env.MONGODB_URL)
-  .then(() => console.log("Database Connected"))
-  .catch((error) => console.log(error.message));
-
-module.exports = mongoConnect;
